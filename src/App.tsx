12b6b6cc61c2f4da9026bb651e2541927e17edfe
d2Dreamer <41088824+d2Dreamer@users.noreply.github.com>
2023-08-23 09:38:08 +0100

    Initialize repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Tailwind Test
+    </h1>
+  );  
+}
+
+export default App;
