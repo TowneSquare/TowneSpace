fc0f476908cce01d900d4c9bafbd4ba2e6d99428
alenzer <91666375+alenzer@users.noreply.github.com>
2023-09-01 02:47:14 -0400

    home

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Search from "./search";
 import Menu from "./menu";
 import Address from "./address";
@@ -11,12 +11,12 @@ import ConnectButton from "./connect_button";
 const Header = () => {
    const isOpen = useAppSelector(state => state.dialogState.bWalletPanel);
    const dispatch = useAppDispatch();
-
+   const navigate = useNavigate();
    return (
       <>
-         <div className="w-full h-auto md:h-[92px] flex flex-col md:flex-row justify-normal md:justify-between items-center bg-gray-dark-2 px-2 md:px-12 py-4 md:py-0 gap-4">
+         <div className="w-full h-auto md:h-[92px] flex flex-col md:flex-row justify-normal md:justify-between items-center bg-gray-dark-2 px-2 md:px-12 py-4 md:py-0 gap-4 z-10">
             <div className="flex items-center gap-4 md:gap-12">
-               <img src="/logo.svg" className="w-154.42 h-16.65" />
+               <img src="/logo.svg" className="w-[154.42px] h-[16.65px] cursor-pointer" onClick={() => navigate("/")} />
                <Search />
             </div>
             <div className="h-full flex items-center justify-between md:justify-normal md:gap-12">
@@ -42,7 +42,6 @@ const Header = () => {
                <div className="absolute top-6 right-6 w-4 h-4 cursor-pointer" onClick={() => dispatch(toggleWalletPanel(false))}>
                   <p className="text-3xl font-bold">×</p>
                </div>
-
             </div>
          </div>
       </>
