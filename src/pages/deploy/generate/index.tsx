09990c97b205893e70f9bb1c60534fd4f1f05899
alenzer <91666375+alenzer@users.noreply.github.com>
2023-09-06 04:39:18 -0400

    generate on deploy

--- src/pages/deploy/generate/index.tsx
+++ src/pages/deploy/generate/index.tsx
@@ -1,6 +1,42 @@
+import PrimaryButton from "../../../components/primary_button";
+import ButtonStatus from "../../../type/button_status";
+
 const Generate = () => {
-   return(
-      <div>Generate</div>
+   return (
+      <div className="flex flex-col gap-8 items-center mt-12">
+         <div className="w-1/3 p-4 flex gap-8 border border-gray-light-3 rounded-md">
+            <img src="/deploy/deploy-contract.png" alt="deploy" />
+            <div className="py-4 flex flex-col justify-between">
+               <div>
+                  <p className="text-xl font-semibold">
+                     Deploy contract
+                  </p>
+                  <p className="mt-1">
+                     Deploy the contract to Aptos Tesnet or Mainnet.
+                  </p>
+               </div>
+               <PrimaryButton type={ButtonStatus.active} className="px-1 w-[200px]">
+                  Deploy Contract
+               </PrimaryButton>
+            </div>
+         </div>
+         <div className="w-1/3 p-4 flex gap-8 border border-gray-light-3 rounded-md">
+            <img src="/deploy/download-assets.png" alt="deploy" />
+            <div className="py-4 flex flex-col justify-between">
+               <div>
+                  <p className="text-xl font-semibold">
+                     Download asset
+                  </p>
+                  <p className="mt-1">
+                     Download the set of images that will be created upon minting through the smart contract.
+                  </p>
+               </div>
+               <PrimaryButton type={ButtonStatus.active} className="px-1 w-[200px]">
+                  Dowload assets
+               </PrimaryButton>
+            </div>
+         </div>
+      </div>
    )
 };
 
