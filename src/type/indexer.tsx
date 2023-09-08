1f8b4084303683af0021bde3f648705bc59ac92d
aladeenb <aladeen.btk@gmail.com>
2023-09-08 20:13:03 +0200

    Added query function to get token data

--- src/type/indexer.tsx
+++ src/type/indexer.tsx
@@ -1,7 +1,4 @@
 // query 
-export type CollectionsQueryResponse = {
-    studio_collections: Array<CollectionIndexer>
-}
 
 export type CollectionIndexer = {
     creator_address: string,
@@ -10,6 +7,14 @@ export type CollectionIndexer = {
     description: string,
     uri: string,
     total_supply: number,   
+}
+
+export type TokenIndexer = {
+    owner_address: string,
+    address: string,
+    name: string,
+    description: string,
+    uri: string,
 }
 
 export const COLLECTIONS_QUERY = 
@@ -39,8 +44,34 @@ export const COLLECTIONS_QUERY =
       }
     `
 
+export const TOKENS_QUERY =
+      `
+      #import "./CurrentTokenOwnershipFieldsFragment";
+      query getOwnedTokens(
+        $where_condition: current_token_ownerships_v2_bool_exp!
+        $offset: Int
+        $limit: Int
+        $order_by: [current_token_ownerships_v2_order_by!]
+        ) {
+          current_token_ownerships_v2(where: $where_condition, offset: $offset, limit: $limit, order_by: $order_by) {
+            ...CurrentTokenOwnershipFields
+          }
+        }
+      `
+
 // response
+
+export type CollectionsQueryResponse = {
+  studio_collections: Array<CollectionIndexer>
+}
+
+export type TokensQueryResponse = {
+  studio_tokens: Array<TokenIndexer>
+}
+
 export type CollectionsResponse = Array<Collection>
+
+export type TokensResponse = Array<Token>
 
 export type Collection = {
     creator_address: string,
@@ -49,4 +80,12 @@ export type Collection = {
     description: string,
     uri: string,
     total_supply: number,
+}
+
+export type Token = {
+    owner_address: string,
+    address: string,
+    name: string,
+    description: string,
+    uri: string,
 }
