24ab3ce1c15ecc60448cbf7d394ea29c4dafafba
d2Dreamer <41088824+d2Dreamer@users.noreply.github.com>
2023-09-08 15:01:12 +0100

    Migrate Text

--- src/pages/migrate/index.tsx
+++ src/pages/migrate/index.tsx
@@ -1,9 +1,15 @@
 import Header from "../../components/header";
+import Text from "./text";
 
 const Migrate  = () => {
     return (
        <div className="">
           <Header />
+          <div className="pt-9 px-2 md:px-12">
+            <div className="flex flex-col md:flex-row gap-4 md:gap-0 md:justify-between">
+               <Text />
+            </div>
+         </div>
        </div>
     )
  }
