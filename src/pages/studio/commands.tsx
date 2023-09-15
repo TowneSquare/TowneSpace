dab15860021eb470b9f4c844a0ffebf9c877ac4d
alenzer <91666375+alenzer@users.noreply.github.com>
2023-09-15 11:40:57 -0400

    create popup page

--- src/pages/studio/commands.tsx
+++ src/pages/studio/commands.tsx
@@ -1,18 +1,23 @@
-import { useNavigate } from "react-router-dom";
 import PrimaryButton from "../../components/primary_button";
 import ButtonStatus from "../../type/button_status";
+import { useAppDispatch } from "../../state/hooks";
+import { toggleCreatePanel } from "../../state/dialog";
 
 const Commands = () => {
-   const navigate = useNavigate();
+   const dispatch = useAppDispatch();
    return (
       <div className="flex flex-col md:flex-row gap-2 md:gap-12 items-center">
          <div className="flex gap-2 md:gap-12">
-            <PrimaryButton type={ButtonStatus.active} className="!px-12 py-3" onClick={() => navigate('/create')}>
+            <PrimaryButton
+               type={ButtonStatus.active}
+               className="!px-12 py-3"
+               onClick={() => dispatch(toggleCreatePanel(true))}
+            >
                Create
             </PrimaryButton>
          </div>
       </div>
-   )
-}
+   );
+};
 
 export default Commands;
--- src/state/dialog.tsx
+++ src/state/dialog.tsx
@@ -1,19 +1,21 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface dialogStates {
    bTraitPanel: boolean;
    bRemovePanel: boolean;
    bWalletPanel: boolean;
-};
+   bCreatePanel: boolean;
+}
 
 const initialState: dialogStates = {
    bTraitPanel: false,
    bRemovePanel: false,
    bWalletPanel: false,
-}
+   bCreatePanel: false,
+};
 
 export const dialogSlice = createSlice({
-   name: 'dialog',
+   name: "dialog",
    initialState,
    reducers: {
       toggleTraitPanel: (state, action: PayloadAction<boolean>) => {
@@ -24,18 +26,14 @@ export const dialogSlice = createSlice({
       },
       toggleWalletPanel: (state, action: PayloadAction<boolean>) => {
          state.bWalletPanel = action.payload;
-      }
+      },
+      toggleCreatePanel: (state, action: PayloadAction<boolean>) => {
+         state.bCreatePanel = action.payload;
+      },
    },
-   extraReducers: (builder) => {
-
-   },
+   extraReducers: (builder) => {},
 });
 
-export const {
-   toggleTraitPanel,
-   toggleRemovePanel,
-   toggleWalletPanel,
-} = dialogSlice.actions;
+export const { toggleTraitPanel, toggleRemovePanel, toggleWalletPanel, toggleCreatePanel } =
+   dialogSlice.actions;
 export default dialogSlice.reducer;
-
-
