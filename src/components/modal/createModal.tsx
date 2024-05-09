aa436c560d23cb97d2832e930b01a31f2bcc5be3
codewizard870 <alenzer0902@gmail.com>
2024-05-10 00:33:12 +0300

    feat tooltip to question mark

--- src/components/modal/createModal.tsx
+++ src/components/modal/createModal.tsx
@@ -13,11 +13,12 @@ const CreateModal = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const getImageSrc = () => {
+        const timeStamp = new Date().getTime();
         switch (currentIndex) {
             case 1:
-                return "/create/game-background.svg";
+                return `/create/game-background.svg?${timeStamp}`;
             default:
-                return "/create/dynamic-background.svg";
+                return `/create/dynamic-background.svg?${timeStamp}`;
         }
     };
 
@@ -28,7 +29,7 @@ const CreateModal = () => {
 
     return (
         <div className={`${isOpen ? "block" : "hidden"} fixed z-[100] inset-0 flex justify-center items-center bg-[#00000050]`}>
-            <div className="w-full xl:w-[1023px] mx-4 h-fit flex  bg-gray-dark-1 rounded-[10px]">
+            <div className="w-full lg:w-[1023px] mx-4 h-fit flex  bg-gray-dark-1 rounded-[10px]">
                 <div className="flex flex-col md:w-[373px] bg-gray-dark-2 px-2 md:px-6 py-10 rounded-l-[10px]">
                     <p className="text-2xl md:text-[29px] text-white font-bold">Create</p>
                     <div className="mt-20 flex flex-col w-full">
@@ -54,13 +55,13 @@ const CreateModal = () => {
                         <p className="text-base md:text-xl">
                             Dynamic PFPs will change their appearance based on their traits and metadata changes.
                         </p>
-                        <div className="w-full flex items-center gap-14 justify-end">
+                        <div className="w-full flex items-center gap-6 sm:gap-14 justify-end">
                             <Link to="/">
                                 <p className="text-sm md:text-base text-primary-light font-semibold whitespace-nowrap">
                                     Learn More
                                 </p>
                             </Link>
-                            <PrimaryButton className="w-[153px]" type={ButtonStatus.active} onClick={handleContinue}>
+                            <PrimaryButton className="w-[100px] sm:w-[153px]" type={ButtonStatus.active} onClick={handleContinue}>
                                 Create
                             </PrimaryButton>
                         </div>
