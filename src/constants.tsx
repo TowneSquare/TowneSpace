efc526e83b27f462db9ff6a44b6289d97f0f3b94
Aladeenb <alabtk@gmail.com>
2024-05-24 14:56:51 +0200

    Addedd APIs to get creation data

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,6 +1,98 @@
-export const STUDIO_ADDRESS =
-  '0x07ff0308d16a5041ace8e7e306a60b2946b6a4aee0e7e923b8f20e49bf3508ce';
+/** 
+ * ========
+ * PACKAGES 
+ * ========
+*/
 
-// hardcoded to devnet for now.
-export const NODE_URL =
-  process.env.APTOS_NODE_URL || 'https://fullnode.devnet.aptoslabs.com';
+import { e } from "mathjs"
+
+/** TESTNET */
+
+export const COMPOSABLE_TOKEN_TESTNET = '0x51ad96e82f8a4a5f0e32b7a4ea25ed7672661661a86cac795e41bb8d26cf0721'
+export const TOKEN_MINTER_TESTNET = '0x07ff0308d16a5041ace8e7e306a60b2946b6a4aee0e7e923b8f20e49bf3508ce'
+export const TOWNESPACE_TESTNET = '0x75718db800d965ab9f2fc0bf727f063ea5838fe05e6ced37af2f31f6151cfb2b'
+
+/** MAINNET */
+
+export const COMPOSABLE_TOKEN_MAINNET = ''
+export const TOKEN_MINTER_MAINNET = ''
+export const TOWNESPACE_MAINNET = ''
+
+/** 
+ * =======
+ * MODULES
+ * =======
+*/
+
+/** Composable Token */
+export const COMPOSABLE_TOKEN = `composable_token`
+export const STUDIO = `studio`
+
+/** Townespace */
+
+export const TOWNESPACE_STUDIO = `studio`
+export const BATCH_MINT = `batch_mint`
+
+/** Token Minter */
+
+/** 
+ * =========
+ * READ APIs
+ * =========
+*/
+
+/** Composable Token */
+
+/** Townespace */
+
+/** Token Minter */
+
+/** 
+ * =========
+ * WRITE APIs
+ * =========
+*/
+
+/** Composable Token */
+
+/** Townespace */
+
+/** Token Minter */
+
+/** 
+ * ======
+ * EVENTS
+ * ======
+*/
+
+/** Composable Token */
+export const COLLECTION_CREATED = `CollectionCreatedEvent`
+export const COMPOSABLE_TOKEN_CREATED = `ComposableTokenCreatedEvent`
+export const TRAIT_TOKEN_CREATED = `TraitTokenCreatedEvent`
+export const DA_CREATED = `DACreatedEvent`
+
+export const TRAIT_EQUIPPED = `TraitEquippedEvent`
+export const TRAIT_UNEQUIPPED = `TraitUnequippedEvent`
+export const DA_EQUIPPED = `DigitalAssetEquippedEvent`
+export const DA_UNEQUIPPED = `DigitalAssetUnequippedEvent`
+export const FA_EQUIPPED = `FAEquippedEvent`
+export const FA_UNEQUIPPED = `FAUnequippedEvent`
+export const FA_TRANSFERRED = `FATransferredEvent`
+
+export const TRANSFER_FROZEN = `TransferFrozenEvent`
+export const TRANSFER_UNFROZEN = `TransferUnfrozenEvent`
+
+export const TOKEN_BURNED = `TokenBurnedEvent`
+export const TOKEN_DESCRIPTION_UPDATED = `TokenDescriptionUpdatedEvent`
+export const TOKEN_NAME_UPDATED = `TokenNameUpdatedEvent`
+export const TOKEN_URI_UPDATED = `TokenUriUpdatedEvent`
+export const PROPERTY_ADDED = `PropertyAddedEvent`
+export const TYPED_PROPERTY_ADDED = `TypedPropertyAddedEvent`
+export const PROPERTY_REMOVED = `PropertyRemovedEvent`
+export const PROPERTY_UPDATED = `PropertyUpdatedEvent`
+
+
+/** Townespace */
+
+/** Token Minter */
+
