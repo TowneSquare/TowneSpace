7c97d55f6bace7c5057d36fc43a9aae4e12cd398
Aladeenb <alabtk@gmail.com>
2024-05-28 14:56:08 +0200

    Added query function to get owned v2 collections and refactored functions names

--- src/api/queries.ts
+++ src/api/queries.ts
@@ -12,20 +12,35 @@ export type TokenV2Fields = {
     description: string;
     token_uri: string;
   };
+
+export type CollectionV2Fields = {
+    collection_id: string;
+    collection_name: string;
+    collection_uri: string;
+  };
   
 type TokenV2FieldsIndexerResponse = {
     current_token_ownerships_v2: Array<OwnedTokenV2Indexer>;
   };
+
+type CollectionV2FieldsIndexerResponse = {
+current_collection_ownership_v2_view: Array<CollectionV2Fields>;
+};
+
 export type OwnedTokenV2Indexer = {
     current_token_data: TokenV2Fields;
   };
+
+export type OwnedCollectionV2Indexer = {
+current_collection_data: CollectionV2Fields;
+};
 
 /**
  * 
  * Queries
  * 
  */
-export const OWNED_TOKENS_V2_QUERY = `
+export const OWNED_V2_TOKENS_QUERY = `
     query MyQuery($offset: Int!, $limit: Int, $account_address: String) {
       current_token_ownerships_v2(
         where: {owner_address: {_eq: $account_address}}
@@ -43,6 +58,21 @@ export const OWNED_TOKENS_V2_QUERY = `
       }
     }
   `;
+
+export const OWNED_V2_COLLECTIONS_QUERY = `
+    query MyQuery($offset: Int!, $limit: Int, $account_address: String) {
+        current_collection_ownership_v2_view(
+        where: {owner_address: {_eq: $account_address}}
+        limit: $limit
+        offset: $offset
+        order_by: {collection_name: asc}
+        ) {
+        collection_id
+        collection_name
+        collection_uri
+        }
+    }
+    `;
 export class Queries { 
     readonly provider: Aptos;
 
@@ -59,7 +89,15 @@ export class Queries {
           query: graphqlQuery,
         });
       }
-
+    
+      /**
+       * 
+       * Get owned v2 tokens
+       * @param offset 
+       * @param limit 
+       * @param account_address 
+       * @returns 
+       */
     async getOwnedTokensV2(
         offset: number,
         limit: number,
@@ -72,7 +110,7 @@ export class Queries {
         };
 
         const response: TokenV2FieldsIndexerResponse = await this.queryIndexer(
-            OWNED_TOKENS_V2_QUERY,
+            OWNED_V2_TOKENS_QUERY,
             variables
         );
 
@@ -84,4 +122,35 @@ export class Queries {
         return tokens;
     }
     
+    /**
+     * 
+     * Get owned v2 collections
+     * @param offset 
+     * @param limit 
+     * @param account_address 
+     * @returns 
+     */
+    async getOwnedCollectionsV2(
+        offset: number,
+        limit: number,
+        account_address: string
+    ): Promise<Array<CollectionV2Fields>> {
+        const variables = {
+            offset,
+            limit,
+            account_address,
+        };
+
+        const response: CollectionV2FieldsIndexerResponse = await this.queryIndexer(
+            OWNED_V2_COLLECTIONS_QUERY,
+            variables
+        );
+
+        const collections = [];
+
+        for (const collection of response.current_collection_ownership_v2_view) {
+            collections.push(collection);
+        }
+        return collections;
+    }
 }
