9ef0b5a462b52288ee7fc76103c7382dd3c76bd8
Aladeenb <alabtk@gmail.com>
2024-05-30 00:10:48 +0200

    Added hooks to get types of a list of tokens

--- src/hooks/useTokenType.ts
+++ src/hooks/useTokenType.ts
@@ -1,7 +1,13 @@
 import { Aptos, InputViewFunctionData } from "@aptos-labs/ts-sdk"
 import { COMPOSABLE_TOKEN_TYPE , TRAIT_TOKEN_TYPE} from "../constants";
 
-
+/**
+ * 
+ * Get the trait type
+ * @param aptos 
+ * @param tokenObject 
+ * 
+ */
 export const useTraitType = async (aptos: Aptos, tokenObject: string) => {
     const payload: InputViewFunctionData = {
         function: `0x4::token::description`,
@@ -14,6 +20,13 @@ export const useTraitType = async (aptos: Aptos, tokenObject: string) => {
     console.log(response);
 };
 
+/**
+ *
+ * Get the composable type
+ * @param aptos
+ * @param tokenObject
+ * 
+ */
 export const useComposableType = async (aptos: Aptos, tokenObject: string) => {
     const payload: InputViewFunctionData = {
         function: `0x4::token::description`,
@@ -25,3 +38,49 @@ export const useComposableType = async (aptos: Aptos, tokenObject: string) => {
     });
     console.log(response);
 };
+
+/**
+ *
+ * Get the trait types; Useful for getting multiple trait types
+ * @param aptos
+ * @param tokenObjects
+ * 
+ */
+export const useTraitTypes = async (aptos: Aptos, tokenObjects: string[]) => {
+    let results = [];
+    for (const tokenObject of tokenObjects) {
+        const payload: InputViewFunctionData = {
+            function: `0x4::token::description`,
+            typeArguments: [TRAIT_TOKEN_TYPE],
+            functionArguments: [tokenObject],
+        };
+        const response = await aptos.view({
+            payload,
+        });
+        results.push(response);
+    }
+    console.log(results);
+}
+
+/**
+ *
+ * Get the composable types; Useful for getting multiple composable types
+ * @param aptos
+ * @param tokenObjects
+ * 
+ */
+export const useComposableTypes = async (aptos: Aptos, tokenObjects: string[]) => {
+    let results = [];
+    for (const tokenObject of tokenObjects) {
+        const payload: InputViewFunctionData = {
+            function: `0x4::token::description`,
+            typeArguments: [COMPOSABLE_TOKEN_TYPE],
+            functionArguments: [tokenObject],
+        };
+        const response = await aptos.view({
+            payload,
+        });
+        results.push(response);
+    }
+    console.log(results);
+}
