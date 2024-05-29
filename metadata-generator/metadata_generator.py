9ad0847b4180c8fc8a30174f5e6277c7a08f74b4
Aladeenb <alabtk@gmail.com>
2024-05-29 23:42:41 +0200

    Fix double indexing format

--- metadata-generator/metadata_generator.py
+++ metadata-generator/metadata_generator.py
@@ -3,14 +3,16 @@ import shutil
 
 folder_path = '/Users/maclay/Code/TowneSpace/metadata-generator/Cool Sloth'
 mirror_folder_path = '/Users/maclay/Code/TowneSpace/metadata-generator/generated'
-output_md_path = '/Users/maclay/Code/TowneSpace/metadata-generator/generated_names.md'
+output_md_path = '/Users/maclay/Code/TowneSpace/metadata-generator/generated_for_payload.md'
 
 def generate_image_metadata(folder_path, mirror_folder_path):
     folder_name = os.path.basename(folder_path)
     images_metadata = []
     image_names_csv = []  # List to store image names for CSV
     uris = []  # List to store image URIs
+    suffixes = []  # List to store suffixes for image names
     index = 1  # Initialize index for images
+    descriptions = []  # List to store descriptions
     occurrence_num = 1  # Number of times to repeat each image
     
     # Create the mirror folder if it doesn't exist
@@ -20,10 +22,11 @@ def generate_image_metadata(folder_path, mirror_folder_path):
     # Copy images directly into the generated folder
     for filename in os.listdir(os.path.join(folder_path, 'Body')):
         if filename.endswith(".png"):
-            new_image_name = f"{folder_name} #{index}.png"
+            new_image_name = f"{folder_name}"
             # parse through the folder name and change sapces to %20
             parsed_folder_name = folder_name.replace(" ", "%20")
-            image_uri = f"{parsed_folder_name}%20%23{index}.png"
+            image_uri = f"{parsed_folder_name}"
+            description = f"{folder_name}"
             image_metadata = {
                 "name": new_image_name,
                 "description": "",
@@ -40,6 +43,8 @@ def generate_image_metadata(folder_path, mirror_folder_path):
             shutil.copyfile(os.path.join(folder_path, 'Body', filename), mirror_image_path)
             image_names_csv.append(new_image_name)  # Add image name to CSV list
             uris.append(image_uri)  # Add image URI to list
+            suffixes.append(f"")
+            descriptions.append(description)
             index += 1  # Increment index for the next image
 
     # Traverse through the rest of the folders
@@ -49,9 +54,10 @@ def generate_image_metadata(folder_path, mirror_folder_path):
             for filename in os.listdir(subfolder_path):
                 if filename.endswith(".png"):
                     image_name = filename.split('.')[0]  # Remove file extension from filename
-                    new_image_name = f"{image_name} #{index}.png"
+                    new_image_name = f"{image_name}"
                     parsed_image_name = image_name.replace(" ", "%20")
-                    image_uri = f"{parsed_image_name}%20%23{index}.png"
+                    image_uri = f"{parsed_image_name}"
+                    description = f"{dir_name}"
                     image_metadata = {
                         "name": new_image_name,
                         "description": "",
@@ -68,18 +74,35 @@ def generate_image_metadata(folder_path, mirror_folder_path):
                     shutil.copyfile(os.path.join(subfolder_path, filename), mirror_image_path)
                     image_names_csv.append(new_image_name)  # Add image name to CSV list
                     uris.append(image_uri)  # Add image URI to list
+                    suffixes.append(f"")
+                    descriptions.append(description)
                     index += 1  # Increment index for the next image
 
-    return images_metadata, image_names_csv, uris  # Return image metadata, CSV data and URIs
+    return images_metadata, image_names_csv, uris, suffixes, descriptions, index-1  # Return image metadata, CSV data, URI, suffixes, descriptions and total count
 
-metadata, csv_data, uris = generate_image_metadata(folder_path, mirror_folder_path)
+metadata, csv_data, uris, suffixes, descriptions, count = generate_image_metadata(folder_path, mirror_folder_path)
 
 # Generate Markdown file with list of generated names in the specified format
 with open(output_md_path, 'w') as md_file:
+    md_file.write('Token descriptions:\n')
+    md_file.write('["')
+    md_file.write('", "'.join(descriptions))
+    md_file.write('"]\n')
+    md_file.write('\n')
+    md_file.write('Token URIs:\n')
+    md_file.write('["')
+    md_file.write('", "'.join(uris))
+    md_file.write('"]\n')
+    md_file.write('\n')
+    md_file.write('Token names:\n')
     md_file.write('["')
     md_file.write('", "'.join(csv_data))
     md_file.write('"]\n')
     md_file.write('\n')
+    md_file.write('Token suffixes:\n')
     md_file.write('["')
-    md_file.write('", "'.join(uris))
+    md_file.write('", "'.join(suffixes))
     md_file.write('"]\n')
+    md_file.write('\n')
+    md_file.write('Total count: ')
+    md_file.write(str(count))
