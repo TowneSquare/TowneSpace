b89dc8eee619b4a661dd26d0239255c967fa0c93
codewizard870 <alenzer0902@gmail.com>
2024-06-10 19:49:36 -0400

    fix on panel

--- src/pages/studio/collections/panel.tsx
+++ src/pages/studio/collections/panel.tsx
@@ -2,10 +2,10 @@ import CollectionCard from '../../../components/collection_card';
 import { useAppSelector } from '../../../state/hooks';
 
 const Panel = () => {
-  const collections = useAppSelector((state) => state.tokensState.collections);
-  console.log(collections)
+  const collections = useAppSelector((state) => state.tokensState.myCollections);
+
   return (
-    <div className="flex h-full flex-wrap gap-4 items-baseline">
+    <div className="flex flex-wrap items-baseline h-full gap-4">
       {collections.map((collection, index) => (
         <CollectionCard data={collection} index={index} key={index} />
       ))}
