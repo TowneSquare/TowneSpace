764a180ba1d698cab63aa0a4581af9179c8b1453
codewizard870 <alenzer0902@gmail.com>
2024-06-11 13:54:49 -0400

    fetch all nfts

--- src/pages/studio/tokens/collection_panel/index.tsx
+++ src/pages/studio/tokens/collection_panel/index.tsx
@@ -22,7 +22,7 @@ const CollectionPanel = () => {
       <Search />
       {collections.map((collection, index) => (
         <div
-          className={`px-2 py-2 flex items-center gap-2 rounded-lg hover:bg-gray-dark-1 cursor-pointer ${collection.collection_name == currentCollection?.collection_name ? 'bg-gray-dark-1' : ''}`}
+          className={`px-2 py-2 flex items-center gap-2 rounded-lg hover:bg-gray-dark-1 cursor-pointer ${collection.collection_id == currentCollection?.collection_id ? 'bg-gray-dark-1' : ''}`}
           key={index}
           onClick={() => onChooseCollection(collection)}
         >
